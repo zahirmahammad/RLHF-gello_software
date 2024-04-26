47f5dc6f9ebed35224299558ac4c28dbfb1704f7
Sachin Jadhav <sjd3333@umd.edu>
2024-04-26 14:14:15 -0400

    updated positions

--- gello/agents/gello_agent.py
+++ gello/agents/gello_agent.py
@@ -81,7 +81,7 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
         joint_ids=(1, 2, 3, 4, 5, 6),
         joint_offsets=
         (
-            -1*np.pi/2, 
+            1*np.pi/2, 
             2*np.pi/2, 
             1*np.pi/2, 
             2*np.pi/2, 
