fbd77e10eba22621a0fce25357dcfb08b6b9bbc2
Sachin Jadhav <sjd3333@umd.edu>
2024-05-23 15:00:38 -0400

    updated csvs

--- gello/dm_control_tasks/robosuite_sac/csv_to_hdf5_images.py
+++ gello/dm_control_tasks/robosuite_sac/csv_to_hdf5_images.py
@@ -4,18 +4,55 @@ import numpy as np
 import pandas as pd
 import glob 
 import os
-from PIL import Image
 import cv2
 
 # Create sample data
-csv_folder = "/home/sj/Downloads/csv"
+# csv_folder = "/home/sj/Downloads/csv"
+csv_folder = "/home/sj/gello_software/csv"
 
 image_folder = "/home/sj/Downloads/images"
 
 low_dim_data = []
+next_low_dim_data = []
+
+action_dataset = []
+next_action_dataset = []
+
+robot_end_eff_pos = []
+next_robot_end_eff_pos = []
+
+robot_end_eff_quat = []
+next_robot_end_eff_quat = []
+
+states_data = []
+next_states_data = []
+
 for csv_file in glob.glob(os.path.join(csv_folder, '*.csv')):
-    dataset = pd.read_csv(csv_file, usecols=range(10, 16))
+    dataset = pd.read_csv(csv_file, usecols=range(0, 6), skiprows=1, engine='python').astype(np.float64)
+    next_dataset = pd.read_csv(csv_file, usecols=range(0, 6), skipfooter=1, engine='python').astype(np.float64)
+
+    action_dataframe = pd.read_csv(csv_file, usecols=range(6, 12), skiprows=1, engine='python').astype(np.float64)
+    
+    robot_end_eff_pos_df = pd.read_csv(csv_file, usecols=range(6, 9), skiprows=1, engine='python').astype(np.float64)
+    next_robot_end_eff_pos_df = pd.read_csv(csv_file, usecols=range(6, 9), skipfooter=1, engine='python').astype(np.float64)   
+    
+    robot_end_eff_quat_df = pd.read_csv(csv_file, usecols=range(12, 16), skiprows=1, engine='python').astype(np.float64)
+    next_robot_end_eff_quat_df = pd.read_csv(csv_file, usecols=range(12, 16), skipfooter=1, engine='python').astype(np.float64)
+    
+    states_dataframe = pd.read_csv(csv_file, usecols=range(0, 16), skiprows=1, engine='python').astype(np.float64)
+
     low_dim_data.append(dataset)
+    next_low_dim_data.append(next_dataset)
+
+    action_dataset.append(action_dataframe)
+
+    robot_end_eff_pos.append(robot_end_eff_pos_df)
+    next_robot_end_eff_pos.append(next_robot_end_eff_pos_df)
+
+    robot_end_eff_quat.append(robot_end_eff_quat_df)
+    next_robot_end_eff_quat.append(next_robot_end_eff_quat_df)
+
+    states_data.append(states_dataframe)
 
 it = 0
 image_data = []
@@ -32,9 +69,9 @@ print("Low dim data shape:", low_dim_data[0].shape)
 print("Image data shape:", image_data.shape)
 print("iterating over image data:", it)
 
+
 # Define the data
 total_samples = len(low_dim_data)
-
 env_args = {
     "env_name": "Lift",
     "type": 1,
@@ -77,6 +114,8 @@ env_args = {
 # Define HDF5 file path
 file_path = "/home/sj/Downloads/data.hdf5"
 
+# if not os.path.exists(file_path):
+#     os.makedirs(file_path)
 # Delete the existing HDF5 file if it exists
 if os.path.exists(file_path):
     os.remove(file_path)
@@ -90,15 +129,7 @@ with h5py.File(file_path, "w") as f:
     data_group.attrs["total"] = total_samples
     data_group.attrs["env_args"] = json.dumps(env_args)
 
-    for i in range(len(low_dim_data)-1):
-        rewards_data = [0]*(len(low_dim_data[i])-3) + [1]*3
-        globals()[f'states_demo_{i}'] = np.array(low_dim_data[i])
-        globals()[f'actions_demo_{i}'] = np.random.uniform(-1, 1, (len(low_dim_data[i]), 6))
-        globals()[f'rewards_demo_{i}'] = rewards_data
-        globals()[f'dones_demo_{i}'] = rewards_data
-        globals()[f'obs_demo_{i}'] = np.array(low_dim_data[i][:-1])
-        globals()[f'images_demo_{i}'] = image_data
-        globals()[f'next_obs_demo_{i}'] = np.array(low_dim_data[i][1:])
+    for i in range(len(low_dim_data)):
 
         # object_demo_
         num_rows = low_dim_data[i].shape[0]
@@ -126,11 +157,23 @@ with h5py.File(file_path, "w") as f:
             low, high = column_ranges[col]
             if low > high:  # Ensure the correct range order
                 low, high = high, low
-            object[:, col] = np.random.uniform(low=low, high=high, size=num_rows)
+            object[:, col] = np.random.uniform(low=low, high=high, size=num_rows
+                                               )
 
-        # print("Actions shape:", np.array(globals()[f'actions_demo_{i}']).shape)
+        rewards_data = [0]*(len(low_dim_data[i])-3) + [1]*3
+        globals()[f'states_demo_{i}'] = np.array(states_data[i])
+        globals()[f'actions_demo_{i}'] = np.array(action_dataset[i])
+        globals()[f'rewards_demo_{i}'] = rewards_data
+        globals()[f'dones_demo_{i}'] = rewards_data
+
+        globals()[f'obs_demo_{i}'] = np.array(low_dim_data[i])
+        globals()[f'next_obs_demo_{i}'] = np.array(next_low_dim_data[i])
+
+        globals()[f'images_demo_{i}'] = image_data
 
 
+        print("Actions shape:", np.array(globals()[f'actions_demo_{i}']).shape)
+        
         # Write data for the current trajectory
         demo_group = data_group.create_group(f"demo_{i}")
         demo_group.attrs["num_samples"] = low_dim_data[i].shape[0]
@@ -140,24 +183,22 @@ with h5py.File(file_path, "w") as f:
         demo_group.create_dataset("rewards", data=locals()[f"rewards_demo_{i}"])
         demo_group.create_dataset("dones", data=locals()[f"dones_demo_{i}"])
 
-
         # Write observation data directly
         obs_group = demo_group.create_group("obs")
         obs_group.create_dataset("robot0_joint_pos", data=locals()[f"obs_demo_{i}"])
-        obs_group.create_dataset("robot0_eef_pos", data=np.random.uniform(-1, 1, (num_rows, 3)))  
-        obs_group.create_dataset("robot0_eef_quat", data=np.random.uniform(-1, 1, (num_rows, 4)))  
-        obs_group.create_dataset("robot0_gripper_qpos", data=np.random.uniform(-1, 1, (num_rows, 1)))  
+        obs_group.create_dataset("robot0_eef_pos", data=np.array(robot_end_eff_pos[i])) 
+        obs_group.create_dataset("robot0_eef_quat", data=np.array(robot_end_eff_quat[i]))
         obs_group.create_dataset("object", data=object)
         obs_group.create_dataset("robot0_eye_in_hand_image", data=locals()[f"images_demo_{i}"])
-    
 
+        
         next_obs_group = demo_group.create_group("next_obs")
         next_obs_group.create_dataset("robot0_joint_pos", data=locals()[f"next_obs_demo_{i}"])
-        next_obs_group.create_dataset("robot0_eef_pos", data=np.random.uniform(-1, 1, (num_rows, 3))) 
-        next_obs_group.create_dataset("robot0_eef_quat", data=np.random.uniform(-1, 1, (num_rows, 4)))  
-        next_obs_group.create_dataset("robot0_gripper_qpos", data=np.random.uniform(-1, 1, (num_rows, 1))) 
+        next_obs_group.create_dataset("robot0_eef_pos", data=np.array(next_robot_end_eff_pos[i])) 
+        next_obs_group.create_dataset("robot0_eef_quat", data=np.array(next_robot_end_eff_quat[i]))  
         next_obs_group.create_dataset("object", data=object)
         next_obs_group.create_dataset("robot0_eye_in_hand_image", data=locals()[f"images_demo_{i}"])
-        
+
+
 print("Data has been written to:", file_path)
-print(low_dim_data[0].shape)
+
